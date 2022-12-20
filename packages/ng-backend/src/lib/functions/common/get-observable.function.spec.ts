24be42ce16ce235b8c00541fa9bc72a02bf533f9
vsezol <vsezold@gmail.com>
2022-12-20 22:27:57 +0300

    feat: write tests for get-observable

---
+++ packages/ng-backend/src/lib/functions/common/get-observable.function.spec.ts
@@ -0,0 +1,39 @@
+import { isObservable, of } from 'rxjs';
+import { getObservable } from './get-observable.function';
+import { isPromise } from './is-promise.function';
+
+describe('get-observable.function', () => {
+  it('should return an observable when given an observable', () => {
+    const observable = of('test');
+
+    const result = getObservable(observable);
+
+    expect(isObservable(result)).toBe(true);
+    expect(result).toBe(observable);
+  });
+
+  it('should return an observable when given a promise', () => {
+    const promise = Promise.resolve('test');
+
+    const result = getObservable(promise);
+
+    expect(isObservable(result)).toBe(true);
+    expect(isPromise(result)).toBe(false);
+  });
+
+  it('should return an observable when given a value', () => {
+    const value = 'test';
+
+    const result = getObservable(value);
+
+    expect(isObservable(result)).toBe(true);
+    expect(isPromise(result)).toBe(false);
+  });
+
+  it('should handle null or undefined values correctly', () => {
+    [undefined, null].map(getObservable).forEach((result) => {
+      expect(isObservable(result)).toBe(true);
+      expect(isPromise(result)).toBe(false);
+    });
+  });
+});
