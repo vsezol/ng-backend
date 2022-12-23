23795dceb3ead1542a23a9fe537ab7b2d31e0085
vsezol <vsezold@gmail.com>
2022-12-24 01:06:02 +0300

    feature: add patch-request-decorator tests

--- packages/ng-backend/src/lib/functions/decorators/patch-request.decorator.spec.ts
+++ packages/ng-backend/src/lib/functions/decorators/patch-request.decorator.spec.ts
@@ -1,8 +1,15 @@
 import { HttpRequest } from '@angular/common/http';
 import { HttpMethodName } from '../../declarations/enums/http-method-name.enum';
+import type { MethodHandlerResult } from '../../declarations/types/method-handler-result.type';
 import { PatchRequest } from './patch-request.decorator';
 
 describe('patch-request.decorator', () => {
+  const beginUrl: string = 'BEGIN_URL';
+  const beginHttpRequest: HttpRequest<void> = new HttpRequest<void>(
+    HttpMethodName.GET,
+    beginUrl
+  );
+
   it('should return a MethodDecorator function', () => {
     const result = PatchRequest((request) => request);
 
@@ -10,76 +17,80 @@ describe('patch-request.decorator', () => {
     expect(result).toHaveLength(3);
   });
 
-  it('should modify method by patching first argument data if target function returns void', () => {
+  it('should modify method by patching input HttpRequest if target method returns void', () => {
+    const patchedUrl = 'PATCHED_URL';
     class Fake {
-      @PatchRequest((request) => request)
-      fakeFn(): void {}
+      @PatchRequest((request) =>
+        request.clone({ url: request.url + patchedUrl })
+      )
+      public fakeFn() {}
     }
-    const fakeHttpRequest: HttpRequest<void> = new HttpRequest<void>(
-      HttpMethodName.GET,
-      'fakeUrl'
-    );
 
     const fakeExemplar: Fake = new Fake();
     const result: unknown = fakeExemplar.fakeFn.bind(
       fakeExemplar,
-      fakeHttpRequest
-    )();
-
-    expect(result).toBeInstanceOf(HttpRequest);
-  });
-
-  it('should modify method by patching target function output if output is HttpRequest argument', () => {
-    const returnedFakeUrl: string = 'returnedFakeUrl';
-    class Fake {
-      @PatchRequest((request) => request)
-      fakeFn(): HttpRequest<void> {
-        return new HttpRequest<void>(HttpMethodName.GET, returnedFakeUrl);
-      }
-    }
-    const fakeHttpRequest: HttpRequest<void> = new HttpRequest<void>(
-      HttpMethodName.GET,
-      'fakeUrl'
-    );
-
-    const fakeExemplar: Fake = new Fake();
-    const result: unknown = fakeExemplar.fakeFn.bind(
-      fakeExemplar,
-      fakeHttpRequest
+      beginHttpRequest
     )();
 
     expect(result).toBeInstanceOf(HttpRequest);
     if (result instanceof HttpRequest) {
-      expect(result.url).toBe(returnedFakeUrl);
+      expect(result.url).toBe(beginUrl + patchedUrl);
     }
   });
 
-  it('should modify method by patching target function output if output is HttpRequest argument', () => {
-    const returnedFakeUrl: string = 'returnedFakeUrl';
+  it('should modify method by patching target method output HttpRequest', () => {
+    const patchedUrl = 'PATCHED_URL';
+    const returnedUrl: string = 'RETURNED_VALUE';
     class Fake {
-      @PatchRequest((request) => request)
-      fakeFn(): HttpRequest<void> {
-        return new HttpRequest<void>(HttpMethodName.GET, returnedFakeUrl);
+      @PatchRequest((request) =>
+        request.clone({ url: request.url + patchedUrl })
+      )
+      public fakeFn(): HttpRequest<void> {
+        return new HttpRequest<void>(HttpMethodName.GET, returnedUrl);
       }
     }
-    const fakeHttpRequest: HttpRequest<void> = new HttpRequest<void>(
-      HttpMethodName.GET,
-      'fakeUrl'
-    );
+
+    const result: HttpRequest<unknown> = new Fake().fakeFn();
+
+    expect(result).toBeInstanceOf(HttpRequest);
+    expect(result.url).toBe(returnedUrl + patchedUrl);
+  });
+
+  it('should not patch anything if target method returns custom value', () => {
+    const customValue: string = 'CUSTOM_VALUE';
+    class Fake {
+      @PatchRequest((request) => request)
+      fakeFn(): MethodHandlerResult {
+        return customValue;
+      }
+    }
+
+    expect(new Fake().fakeFn()).toBe(customValue);
+  });
+
+  it('should patch target method output HttpRequest that patch input HttpRequest', () => {
+    const urlPatchedByDecorator = 'URL_PATCHED_BY_DECORATOR';
+    const urlPatchedByMethod = 'URL_PATCHED_BY_METHOD';
+    class Fake {
+      @PatchRequest((request) =>
+        request.clone({ url: request.url + urlPatchedByDecorator })
+      )
+      fakeFn(request: HttpRequest<void>): MethodHandlerResult {
+        return request.clone({ url: request.url + urlPatchedByMethod });
+      }
+    }
 
     const fakeExemplar: Fake = new Fake();
     const result: unknown = fakeExemplar.fakeFn.bind(
       fakeExemplar,
-      fakeHttpRequest
+      beginHttpRequest
     )();
 
     expect(result).toBeInstanceOf(HttpRequest);
     if (result instanceof HttpRequest) {
-      expect(result.url).toBe(returnedFakeUrl);
+      expect(result.url).toBe(
+        beginUrl + urlPatchedByMethod + urlPatchedByDecorator
+      );
     }
   });
-
-  it.todo(
-    'should not patch argument or output if target function returns custom value'
-  );
 });
