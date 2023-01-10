ac7d50ffdd83095b71b9009e30eb77761b92bc74
vsezol <vsezold@gmail.com>
2023-01-10 17:31:51 +0300

    feat: add docs for ng-backend

--- packages/ng-backend/src/lib/declarations/proxies/auto-response.proxy.ts
+++ packages/ng-backend/src/lib/declarations/proxies/auto-response.proxy.ts
@@ -9,6 +9,46 @@ interface AutoResponseStructure {
 
 const target: AutoResponseStructure = <AutoResponseStructure>(() => VOID);
 
+/**
+ * Proxy that groups decorators {@link AutoResponseFull} and {@link AutoResponseBody} in itself for more convenient use
+ *
+ * @group Decorators
+ *
+ * @example
+ * Sending response with options
+ * ```ts
+ * @Controller('todos')
+ * @Disabled.controller
+ * class TodoController {
+ *  @Get()
+ *  @AutoResponse({
+ *    body: {
+ *      name: 'Vsevolod',
+ *      age: 20
+ *    },
+ *    status: 200
+ *  })
+ *  @Disabled.handler
+ *  public get(): void {}
+ * }
+ * ```
+ *
+ * @example
+ * Sending response with body
+ * ```ts
+ * @Controller('todos')
+ * @Disabled.controller
+ * class TodoController {
+ *  @Get()
+ *  @AutoResponse.body({
+ *    name: 'Vsevolod',
+ *    age: 20
+ *  })
+ *  @Disabled.handler
+ *  public get(): void {}
+ * }
+ * ```
+ */
 export const AutoResponse: AutoResponseStructure =
   new Proxy<AutoResponseStructure>(target, {
     apply: <T>(
--- packages/ng-backend/src/lib/functions/decorators/presets/post.decorator.ts
+++ packages/ng-backend/src/lib/functions/decorators/presets/post.decorator.ts
@@ -4,5 +4,35 @@ import {
   HttpMethodName,
 } from 'base';
 
+/**
+ * Decorator that registers MethodHandler by using {@link HttpMethodDecoratorBuilder}
+ * @group Decorators
+ *
+ * @example
+ * Builder-style
+ * ```ts
+ * @Controller('todos')
+ * class TodoController {
+ *  @Post.id('int')()
+ *  public postById(): void {}
+ *
+ *  @Post.all()
+ *  public patchAll(): void {}
+ * }
+ * ```
+ *
+ * @example
+ * Simple style
+ * ```ts
+ * @Controller('todos')
+ * class TodoController {
+ *  @Post(/(.*)/)
+ *  public postById(): void {}
+ *
+ *  @Post('all')
+ *  public postAll(): void {}
+ * }
+ * ```
+ */
 export const Post: HttpMethodDecoratorBuilder =
   createHttpMethodDecoratorBuilder(HttpMethodName.POST);
