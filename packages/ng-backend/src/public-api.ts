02e7a5ca8960877660b321a8d77fcb8faff8fdfe
vsezol <vsezold@gmail.com>
2023-01-10 11:19:40 +0300

    refactor: add public-api files instead of barrelsby

---
+++ packages/ng-backend/src/public-api.ts
@@ -0,0 +1,16 @@
+export { MethodHandlerInput, MethodHandlerResult } from 'base';
+export { Disabled } from './lib/declarations/classes/presets/disabled.class';
+export { AutoResponse } from './lib/declarations/proxies/auto-response.proxy';
+export { AutoResponseBody } from './lib/functions/decorators/auto-response-body.decorator';
+export { AutoResponseFull } from './lib/functions/decorators/auto-response-full.decorator';
+export { Controller } from './lib/functions/decorators/controller.decorator';
+export { DisabledController } from './lib/functions/decorators/disabled-controller.decorator';
+export { DisabledHandler } from './lib/functions/decorators/disabled-handler.decorator';
+export { Header } from './lib/functions/decorators/header.decorator';
+export { PatchInput } from './lib/functions/decorators/patch-input.decorator';
+export { Delete } from './lib/functions/decorators/presets/delete.decorator';
+export { Get } from './lib/functions/decorators/presets/get.decorator';
+export { Patch } from './lib/functions/decorators/presets/patch.decorator';
+export { Post } from './lib/functions/decorators/presets/post.decorator';
+export { Put } from './lib/functions/decorators/presets/put.decorator';
+export { Redirect } from './lib/functions/decorators/redirect.decorator';
